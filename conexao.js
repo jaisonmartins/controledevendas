6311166bcd5f1025931b9b959a586d5c966f6ae6
Jaison Martins <jaison_martins@hotmail.com>
2025-07-11 18:28:05 -0400

    Add files via upload

---
+++ conexao.js
@@ -0,0 +1,5 @@
+// Arquivo de conexão centralizado para URLs do sistema
+export const SHEET_URL =
+  "https://docs.google.com/spreadsheets/d/1G0whVVjSAihvZ724i8yVNXSYutflna66vidWDXB7iQw/edit";
+export const WEBAPP_URL =
+  "https://script.google.com/macros/s/AKfycbxEpIX-YyjDpvIctlQzmr67NfbdSrN9CX1f6Chufx9OyRak5HBF3uOu_tQ77N68ozIw/exec";
